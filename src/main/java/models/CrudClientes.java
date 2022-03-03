77b531420e59ab6d9fb20ff2cf5c641d4b7ab352
Andrea Tirado <andrea.tangulo@gmail.com>
2022-03-02 17:03:23 -0700

    creacion del metodo insertarEmpleados

--- src/main/java/models/CrudClientes.java
+++ src/main/java/models/CrudClientes.java
@@ -6,43 +6,65 @@ import org.hibernate.cfg.Configuration;
 
 import factory.Factory;
 public class CrudClientes {
+	SessionFactory miSessionFactory;
+	Session miSession;
+	Factory miFactory;
 
 	public static void main(String[] args) {
 		// TODO 
-		SessionFactory miSessionFactory = new Configuration().configure("hibernate.cfg.xml")
-				  .addAnnotatedClass(Cliente.class)
-				  .addAnnotatedClass(Cita.class)
-				  .addAnnotatedClass(Empleado.class)
-				  .addAnnotatedClass(Direccion.class)
-				  .buildSessionFactory();
-		Session miSesion = miSessionFactory.openSession();
-		CrudClientes miCliente = new CrudClientes();
-		miCliente.insertarClientes(miSessionFactory,miSesion);
+		CrudClientes obj = new CrudClientes();
+		obj.insertarClientes("Frida", "6695478362", "e-femartinez@neoris.com");
+		obj.insertarEmpleados("José", "5574936741", "josé.plopez@neoris.com", "Barbero");
 	}
 	
-	public void insertarClientes(SessionFactory miSessionFactory, Session miSesion) {
+	private void inicializarSesiones() {
+		miSessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Cliente.class)
+				.addAnnotatedClass(Cita.class)
+				.addAnnotatedClass(Empleado.class)
+				.addAnnotatedClass(Direccion.class)
+				.buildSessionFactory();
+		miSession = miSessionFactory.openSession();
+		miFactory = new Factory();
+	}
+	
+	public void insertarClientes(String nombre, String telefono, String email) {
+		inicializarSesiones();
 		try {
 			//crear los objetos con las clases de las tablas correspondientes
 			//Date fecha=new Date(120,6,4);
 			//Cliente miCliente = new Cliente("Alan", "6691345271", "al4nsopeña@gmail.com", fecha);
-			Factory miFactory = new Factory();
-			Cliente miCliente = miFactory.CreateCliente("Nelson", "6691345922", "e-nabeas@neoris.com");
+			Cliente miCliente = miFactory.CreateCliente(nombre, telefono, email);
 			/*iniciar transacción*/
-			miSesion.beginTransaction();
+			miSession.beginTransaction();
 			/*insertar cliente*/
-			miSesion.save(miCliente);
+			miSession.save(miCliente);
 			System.out.println(miCliente);
-			miSesion.getTransaction().commit();
+			miSession.getTransaction().commit();
 			System.out.println("¡Cliente insertado! Con id: " + miCliente.getId());
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
-			miSesion.close();
+			miSession.close();
 			miSessionFactory.close();
 		}
 	}
-	
-	public void insertarEmpleados() {
-		
+
+	public void insertarEmpleados(String nombre, String telefono, String correo, String puesto) {
+		inicializarSesiones();
+		try {
+			Empleado miEmpleado = miFactory.CreateEmpleado(nombre, telefono, correo, puesto);
+			miSession = miSessionFactory.openSession();
+			miSession.beginTransaction();
+			miSession.save(miEmpleado);
+			System.out.println(miEmpleado);
+			miSession.getTransaction().commit();
+			System.out.println("¡Empleado Creado! Con id: " + miEmpleado.getId());
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			miSession.close();
+			miSessionFactory.close();
+		}
 	}
 }
